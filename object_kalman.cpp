6438787b01510d7e73bef2700fd60abf637022f9
Morgan French <morgan.french@cwst.com>
2022-06-02 14:45:21 -0700

    Added numerous functions to kalman filter for usability

--- object_kalman.cpp
+++ object_kalman.cpp
@@ -1,27 +1,47 @@
 #include "object_kalman.hpp"
 
 
-ObjectKalman::ObjectKalman(const double noise_covariance, const double map_scalar, const double estimated_covariance) {
-    Noise_Covariance = noise_covariance;
-    Map_Scalar = map_scalar;
-    Estimated_Covariance = estimated_covariance;
-    Error_Covariance = 0.0;
-    Kalman_Gain = 0.0;
-    Kalman_Out = 0.0;
+ObjectKalman::ObjectKalman(const double noise_reduction, const double map_scalar, const double estimated_covariance) {
+  /*Set initial values*/
+  Noise_Reduction = noise_reduction;
+  Map_Scalar = map_scalar;
+  Estimated_Covariance = estimated_covariance;
+
+  /*Reset calculated values*/
+  Error_Value = 0.0;
+  Gain_Value = 0.0;
+  Kalman_Out = 0.0;
 }
 
 /*================================================================*/
 /*Public Methods*/
 /*================================================================*/
-double ObjectKalman::kalmanFilter(const double value_in) {
-    /*Update Kalman_Gain*/
-    Kalman_Gain = (Error_Covariance * Map_Scalar) / (Map_Scalar * Error_Covariance * Map_Scalar + Noise_Covariance);
-    
-    /*Update Error_Covariance*/
-    Error_Covariance = Error_Covariance * (1 - Kalman_Gain * Map_Scalar) + Estimated_Covariance;
+void ObjectKalman::kalmanFilter(const double value_in) {
+  Gain_Value = kalmanCalc_Gain();
+  Error_Value = kalmanCalc_Error();
+  Kalman_Out = kalmanCalc_Out(value_in);
+}
 
-    /*Update estimated Kalman_Out*/
-    Kalman_Out = Kalman_Out + Kalman_Gain * (value_in - Map_Scalar * Kalman_Out);
+double ObjectKalman::kalmanCalc_Error() {
+  return Error_Value * (1 - Gain_Value * Map_Scalar) + Estimated_Covariance;
+}
 
-    return Kalman_Out;
+double ObjectKalman::kalmanCalc_Gain() {
+  return (Error_Value * Map_Scalar) / (Map_Scalar * Error_Value * Map_Scalar + Noise_Reduction);
+}
+
+double ObjectKalman::kalmanCalc_Out(const double value_in) {
+  return Kalman_Out + Gain_Value * (value_in - Map_Scalar * Kalman_Out);
+}
+
+double ObjectKalman::kalmanGet_Error() {
+  return Error_Value;
+}
+
+double ObjectKalman::kalmanGet_Gain() {
+  return Gain_Value;
+}
+
+double ObjectKalman::kalmanGet_Out() {
+  return Kalman_Out;
 }
--- object_kalman.hpp
+++ object_kalman.hpp
@@ -5,15 +5,21 @@ class ObjectKalman {
 
 public:
    ObjectKalman(const double = 10.0, const double = 1.0, const double = 10.0);
-   double kalmanFilter(const double);
+   void kalmanFilter(const double);
+   double kalmanCalc_Error();
+   double kalmanCalc_Gain();
+   double kalmanCalc_Out(const double);
+   double kalmanGet_Error();
+   double kalmanGet_Gain();
+   double kalmanGet_Out();
 
 private:
-    double Kalman_Gain;
+    double Gain_Value;
     double Kalman_Out;
-    double Error_Covariance;
+    double Error_Value;
     double Estimated_Covariance;
     double Map_Scalar;
-    double Noise_Covariance; /*Smaller = more reactive/less filtering, Larger = slower/more filtering*/
+    double Noise_Reduction;
 };
 
 #endif // OBJECTKALMAN_HPP
